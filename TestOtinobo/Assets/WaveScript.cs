2873b6db7268714440cb78421b28a303f9214dcd
uedaryota <mr.335186@gmail.com>
2020-04-27 14:29:55 +0900

    Wave下地(表示&参照用)

---
+++ TestOtinobo/Assets/WaveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveScript : MonoBehaviour
+{
+    public PlayerScript player;
+    public int number;
+    private int score;
+    private int wave = 1;
+    Text text;
+
+    void Start()
+    {
+        text = GameObject.Find("WaveText").GetComponent<Text>();//テキストを探す
+    }
+    
+    void Update()
+    {
+        score = player.scoreline;
+        if (score % number == 0)
+        {
+            wave = score / number + 1;
+        }
+       text.text = wave + "Wave";
+    }
+}
